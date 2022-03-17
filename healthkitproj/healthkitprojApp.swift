52271de6a5bb352fcbc80f4e032179ebe8732dea
Shane Claussen <sclaussen@yahoo.com>
2022-03-17 13:42:41 -0700

    Initial Commit

---
+++ healthkitproj/healthkitprojApp.swift
@@ -0,0 +1,17 @@
+//
+//  healthkitprojApp.swift
+//  healthkitproj
+//
+//  Created by Shane Claussen on 3/17/22.
+//
+
+import SwiftUI
+
+@main
+struct healthkitprojApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
